5d9e5645f7a5ee15ea540bbcf5f9cc7fdd293b14
Andrew Maslankowski <ajm22@illinois.edu>
2022-12-16 21:15:21 -0600

    update_hLRU quick fix, should fix thrashing over old, hot entries

--- sram.py
+++ sram.py
@@ -1,32 +1,28 @@
 import numpy as np
 
 class SRAM:
-    def __init__(self, size, ways):
+    def __init__(self, size, ways, hLRU_size):
         num_sets = int(np.ceil(size/ways))
 
-        self.size           = size
-        self.num_ways       = ways
-        self.num_sets       = num_sets
-        self.tag_arr        = np.zeros(shape=(ways, num_sets), dtype=int)
-        self.valid_arr      = np.zeros(shape=(ways, num_sets), dtype=bool)
-        self.dirty_arr      = np.zeros(shape=(ways, num_sets), dtype=bool)
+        self.size       = size
+        self.num_ways   = ways
+        self.num_sets   = num_sets
+        self.tag_arr    = np.zeros(shape=(ways, num_sets), dtype=int)
+        self.valid_arr  = np.zeros(shape=(ways, num_sets), dtype=bool)
+        self.dirty_arr  = np.zeros(shape=(ways, num_sets), dtype=bool)
         # History-LRU, currently only valid for 2-ways
-        self.histLRU_arr    = np.zeros(shape=(num_sets), dtype=int)
+        self.hLRU_size  = hLRU_size
+        self.hLRU_arr   = np.zeros(shape=(num_sets), dtype=int)
     
     # Find if tag is present, and return corresponding way_addr if so
+    # Does NOT check if tag is valid (maybe it makes more sense to do so here?)
     def find_tag(self, addr, set_addr, set_tag):
         way_addr = -1
 
         # Scan ways for tag match, and valid == True
         if set_tag in self.tag_arr[:, set_addr]:
-            way_addr = np.where(self.tag_arr[:, set_addr] == set_tag)[0][0]
-            
-            # If cacheline valid, update historyLRU
-            if self.valid_arr[way_addr, set_addr] == True:
-                if way_addr == 0:
-                    self.histLRU_arr[set_addr] = max(-3, self.histLRU_arr[set_addr] - 1)
-                elif way_addr == 1: 
-                    self.histLRU_arr[set_addr] = min(4, self.histLRU_arr[set_addr] + 1)
+            if self.valid_arr[way_addr, set_addr]:
+                way_addr = np.where(self.tag_arr[:, set_addr] == set_tag)[0][0]
             
         return way_addr
 
@@ -40,15 +36,13 @@ class SRAM:
         
         # cache hit
         if way_addr != -1:
+            self.update_hLRU(way_addr, set_addr)
             return True, False
         # cache miss
         else:
             # decide which way to evict
-            if self.histLRU_arr[set_addr] <= 0:
-                way_addr = 1
-            else:
-                way_addr = 0
-
+            way_addr = self.decide_hLRU(set_addr)
+            self.update_hLRU(way_addr, set_addr)
             # writes to SRAM cache are abstracted away
             # dirty + read = wb -> load
             # dirty + write = wb -> load -> write
@@ -76,6 +70,7 @@ class SRAM:
                 # Return hit=false, wb=false
                 return False, False
 
+    # Abstract write function, logically dirties the cacheline, but no $line is actually written back
     def dirty(self, way_addr, set_addr):
         self.dirty_arr[way_addr, set_addr] = True
         return 0
@@ -94,3 +89,30 @@ class SRAM:
         self.valid_arr[way_addr, set_addr] = True
         self.dirty_arr[way_addr, set_addr] = False
         return 0
+
+    # Helper function to handle updating historyLRU data structures
+    # (simple counter for 2-way set assoc, 3 counters in btree for 4-way set assoc)
+    def update_hLRU(self, way_addr, set_addr):
+        if self.hLRU_size == 2:
+            if way_addr == 0:
+                self.hLRU_arr[set_addr] = max(-3, self.hLRU_arr[set_addr] - 1)
+            elif way_addr == 1: 
+                self.hLRU_arr[set_addr] = min(4, self.hLRU_arr[set_addr] + 1)
+        
+        # elif self.hLRU_size == 4:
+        #     if way_addr == 0:
+        #         # set_addr - 1 is left child counter, set_addr is parent counter
+        #         self.hLRU_arr[set_addr - 1] = max(-3, self.hLRU[set_addr - 1] - 1)
+        #         self.hLRU_arr[set_addr] = max(-3, self.hLRU[set_addr - 1] - 1)
+        #     elif way_addr == 1:
+        #         self.hLRU_arr[set_addr - 1] = max(-3, self.hLRU[set_addr - 1] - 1)
+        return 0
+
+    def decide_hLRU(self, set_addr):
+        if self.hLRU_size == 2:
+            if self.hLRU_arr[set_addr] <= 0:
+                way_addr = 1
+            else:
+                way_addr = 0
+
+        return way_addr
